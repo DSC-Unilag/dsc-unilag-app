b5056fcb403cbf63ef49ae5303da15f5d4bda265
Oluwatobiloba Ogunye <ogunye4nao@gmail.com>
2019-09-01 11:09:12 +0100

    Added filter chips to home page

---
+++ lib/pages/home/widgets/top_category_card.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class TopCategoryCard extends StatelessWidget {
+  final String title;
+  final String author;
+  final Widget image;
+
+  TopCategoryCard({this.title, this.author, this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 300,
+        child: Card(
+          clipBehavior: Clip.antiAlias,
+          elevation: 2,
+          child: ListTile(
+            leading: Container(
+              width: 50,
+              height: 50,
+              child: this.image,
+            ),
+            title: Container(
+                child: Text(this.title,
+                    style: TextStyle(fontWeight: FontWeight.w600)),
+                margin: EdgeInsets.only(bottom: 10)),
+            subtitle: Text(this.author, style: TextStyle(color: Colors.red)),
+            isThreeLine: true,
+          ),
+        ));
+  }
+}
